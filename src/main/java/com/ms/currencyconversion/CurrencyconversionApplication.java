bb4ef527dc2fd0be1fe9439edf1b34b69af7a73c
kpanda <kpanda70@gmail.com>
2021-08-16 21:23:47 +0530

    changes to call FS service using eureka server

--- src/main/java/com/ms/currencyconversion/CurrencyconversionApplication.java
+++ src/main/java/com/ms/currencyconversion/CurrencyconversionApplication.java
@@ -2,7 +2,12 @@ package com.ms.currencyconversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class CurrencyconversionApplication {
 
